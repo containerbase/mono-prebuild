24caf434b1adaf86430e447ae2c86dc14490ee66
Michael Kriese <michael.kriese@visualon.de>
2026-02-13 11:23:02 +0100

    build: optimise logging

--- bin/builder.sh
+++ bin/builder.sh
@@ -18,17 +18,21 @@ tp=$(create_versioned_tool_path)
 
 check_semver "${TOOL_VERSION}"
 
-echo "Building ${TOOL_NAME} ${TOOL_VERSION} for ${CODENAME}-${ARCH}"
+echo -e "\e[0m\e[32m building ${TOOL_NAME} ${TOOL_VERSION} for ${CODENAME}-${ARCH} ...\e[0m\e[2m"
+echo -e "\e[0m=======================================================\e[2m"
 
 if [[ "${DEBUG}" == "true" ]]; then
   set -x
 fi
 
-echo "------------------------"
-echo "init repo"
+
+echo -e "\e[0m\e[33m prepare repo ...\e[0m\e[2m"
 git reset --quiet --hard "mono-${TOOL_VERSION}"
 git submodule --quiet update --init --recursive
 
+echo -e "\e[0m-------------------------------------------------------"
+
+echo -e "\e[0m\e[33m configure ${TOOL_NAME} ...\e[0m\e[2m"
 ./autogen.sh \
   --prefix="${tp}" \
   --disable-libraries \
@@ -37,22 +41,17 @@ git submodule --quiet update --init --recursive
   --enable-nls=no \
   ;
 
-echo "------------------------"
-echo "------------------------"
-./autogen.sh --help
-echo "------------------------"
-make help
+echo -e "\e[0m-------------------------------------------------------"
 
-echo "------------------------"
-echo "------------------------"
-echo "build ${TOOL_NAME}"
+echo -e "\e[0m\e[33m build ${TOOL_NAME} ...\e[0m\e[2m"
 make -O "-j$(nproc)" all
 make -O "-j$(nproc)" -C runtime install
 
 shell_wrapper mono "${tp}/bin"
 
-echo "------------------------"
-echo "testing"
+echo -e "\e[0m-------------------------------------------------------"
+
+echo -e "\e[0m\e[33m test ${TOOL_NAME} ...\e[0m\e[2m"
 mono --version
 
 # Download the latest stable `nuget.exe` to `/usr/local/bin`
@@ -63,7 +62,11 @@ mono /usr/local/bin/nuget.exe help
 file "${tp}/bin/mono"
 ldd "${tp}/bin/mono"
 
-echo "------------------------"
-echo "create archive"
+echo -e "\e[0m-------------------------------------------------------"
+
+echo -e "\e[0m\e[33m package ${TOOL_NAME} ...\e[0m\e[2m"
 echo "Compressing ${TOOL_NAME} ${TOOL_VERSION} for ${CODENAME}-${ARCH}"
 tar -cJf "/cache/${TOOL_NAME}-${TOOL_VERSION}-${CODENAME}-${ARCH}.tar.xz" -C "$(find_tool_path)" "${TOOL_VERSION}"
+
+echo -e "\e[0m======================================================="
+echo -e "\e[0m\e[32m done\e[0m"
--- bin/install-builder.sh
+++ bin/install-builder.sh
@@ -7,7 +7,10 @@ set -e
 # shellcheck source=/dev/null
 . /usr/local/containerbase/utils/v2/overrides.sh
 
-# add required system packages
+echo -e "\e[0m\e[32m install builder ...\e[0m\e[2m"
+echo -e "\e[0m=======================================================\e[2m"
+
+echo -e "\e[0m\e[33m add required system packages ...\e[0m\e[2m"
 install-apt \
     automake \
     build-essential \
@@ -18,17 +21,26 @@ install-apt \
     pkg-config \
     ;
 
-# prepare mono source
+
+echo -e "\e[0m-------------------------------------------------------"
+
+echo -e "\e[0m\e[33m prepare mono source ...\e[0m\e[2m"
+echo -e "\e[0m-------------------------------------------------------"
 git clone --quiet --recursive https://gitlab.winehq.org/mono/mono.git /usr/src/mono
 
-# create folders
+echo -e "\e[0m-------------------------------------------------------"
+
+echo -e "\e[0m\e[33m create folders ...\e[0m\e[2m"
 create_tool_path > /dev/null
 mkdir /cache
 
-echo "------------------------"
+#echo "------------------------"
 # echo "init repo"
 
 # pushd /usr/src/mono > /dev/null
 # cargo update
 # cargo fetch
 # git reset --hard
+
+echo -e "\e[0m======================================================="
+echo -e "\e[0m\e[32m done\e[0m"
